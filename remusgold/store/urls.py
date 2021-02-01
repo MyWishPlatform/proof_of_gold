322764f4e93f86f400f62d5998e06815184e3c96
dsahdr <dshadr@mail.ru>
2021-02-01 19:13:22 +0300

    login fix

--- remusgold/store/urls.py
+++ remusgold/store/urls.py
@@ -4,6 +4,6 @@ from remusgold.store.views import GroupView, StoreView, UniqueView, ReviewView
 urlpatterns = [
     path('<int:id>/', UniqueView.as_view()),
     path('<str:group>/', GroupView.as_view()),
-    path('review/', ReviewView.as_view()),
+    path('review', ReviewView.as_view()),
     path('', StoreView.as_view())
 ]
