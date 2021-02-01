76f49778f931e686fc4fd7c376a68381c0a0ff83
dsahdr <dshadr@mail.ru>
2021-02-01 17:05:51 +0300

    2 login tests

--- remusgold/account/urls.py
+++ remusgold/account/urls.py
@@ -6,8 +6,7 @@ from rest_framework.authtoken import views
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('<str:token>/', GetView.as_view()),
-    path('api-token-auth', ObtainAuthTokenWithId.as_view()),
-    path('login', views.obtain_auth_token),
+    path('login', ObtainAuthTokenWithId.as_view()),
     path('<str:token>/shipping/', ShippingView.as_view()),
     path('<str:token>/billing/', BillingView.as_view())
 ]
