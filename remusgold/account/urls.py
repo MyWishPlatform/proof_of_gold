60f9fe2a56cfa63ec22f9b94ff6f47e2b27e3105
dsahdr <dshadr@mail.ru>
2021-02-01 16:32:17 +0300

    2 login tests

--- remusgold/account/urls.py
+++ remusgold/account/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
+from django.conf.urls import url
 from remusgold.account.views import RegisterView, GetView, ShippingView, BillingView, ObtainAuthTokenWithId
 from rest_framework.authtoken import views
+from rest_framework.routers import DefaultRouter
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view()),
     path('<str:token>/', GetView.as_view()),
     path('api-token-auth/', ObtainAuthTokenWithId.as_view()),
-    path('login/', views.obtain_auth_token),
+    url(r'^login/$', views.ObtainAuthToken.as_view(), name='login'),
     path('<str:token>/shipping/', ShippingView.as_view()),
     path('<str:token>/billing/', BillingView.as_view())
 ]
