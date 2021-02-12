dcaf308e290644c62e7b45ee14e520de75d1a765
Ameize4 <ameize4@gmail.com>
2021-02-12 17:34:55 +0300

    Add rates checker to project (without migrations yet)

--- remusgold/settings.py
+++ remusgold/settings.py
@@ -171,6 +171,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+RATES_CHECKER_TIMEOUT = 300
 
 try:
     from remusgold.settings_local import *
