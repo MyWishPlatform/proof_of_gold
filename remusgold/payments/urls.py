effdb60b133473bf4681e88b26e8353ee5f21cc0
dsahdr <3180Gmtu>
2021-02-24 14:08:41 +0300

    fix: currency, token in request

--- remusgold/payments/urls.py
+++ remusgold/payments/urls.py
@@ -3,6 +3,6 @@ from remusgold.payments.views import GetPaymentsView, GetSinglePaymentView, Crea
 
 urlpatterns = [
     path('<int:payment_id>/', GetSinglePaymentView.as_view()),
-    path('user/<int:user_id>', GetPaymentsView.as_view()),
+    path('user/<str:token>', GetPaymentsView.as_view()),
     path('<str:token>/', CreatePaymentView.as_view()),
 ]
