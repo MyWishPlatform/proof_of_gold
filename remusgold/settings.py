5e8bc097334481dcdcedf78b08c32fbdd7ea2aa5
dsahdr <72198703+dsahdr@users.noreply.github.com>
2021-03-09 17:20:31 +0300

    static url fix

--- remusgold/settings.py
+++ remusgold/settings.py
@@ -166,7 +166,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/django-static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
