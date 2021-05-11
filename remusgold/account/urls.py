32800b5d30ce09c703568f3254c58f93001d2688
Arsenii Poriadin <37663786+catalyst17@users.noreply.github.com>
2021-05-11 08:57:39 +0300

    Feature/duc 452 (#15)
    * add duc and duc_ref addresses to user model and assign it on registration
    
    * add referral fields to model; implement retrieving ref_code by /account/referral_code and storing it to db with all references
    
    * test creation of user with ref_code; create example of referral_letter.py
    
    * test sending referral letter with freshly-generated voucher

--- remusgold/account/urls.py
+++ remusgold/account/urls.py
@@ -4,13 +4,17 @@ from rest_framework.authtoken import views
 from django_rest_resetpassword.views import reset_password_confirm
 
 from remusgold.account.views import RegisterView, GetView, ShippingView, BillingView, ObtainAuthTokenWithId, \
-register_activate, reset_password_request_token, reset_password_validate_token, GetAddressesView, check_code
+    register_activate, reset_password_request_token, reset_password_validate_token, GetAddressesView, check_code, \
+    total_id_count, get_or_create_referral_code
 
 app_name = 'reset_password'
 
 urlpatterns = [
+    path('register/<str:ref_code>', RegisterView.as_view()),
     path('register/', RegisterView.as_view()),
     path('checkout_addresses/<str:token>', GetAddressesView.as_view()),
+    path('total/', total_id_count),
+    path('<str:token>/referral_code/', get_or_create_referral_code),
     path('<str:token>/', GetView.as_view()),
     path('reset/validate_token/<str:token>/', reset_password_validate_token, name="reset-password-validate"),
     url(r'^reset/confirm/', reset_password_confirm, name="reset-password-confirm"),
